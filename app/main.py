cb129405cf451f8f104eaaed79f10df1f4f8b5f7
RoksanaStanczyk <rstanczyk24@gmail.com>
2022-11-16 22:16:50 +0100

    Add validation

--- app/main.py
+++ app/main.py
@@ -2,21 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import io
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from flask import Flask, request, escape, Response
-
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
 
 @app.route("/plot")
 def plot():
-    a = request.args.get('a')
-    b = request.args.get('b')
-    c = request.args.get('c')
-    xmin = request.args.get('xmin')
-    xmax = request.args.get('xmax')
-    ymin = request.args.get('ymin')
-    ymax = request.args.get('ymax')
+    a = request.args.get('a', default=0)
+    b = request.args.get('b', default=0)
+    c = request.args.get('c', default=0)
+    xmin = request.args.get('xmin', default=-5)
+    xmax = request.args.get('xmax', default=5)
+    ymin = request.args.get('ymin', default=-5)
+    ymax = request.args.get('ymax', default=5)
 
     x = np.linspace(float(xmin), float(xmax))
     y = float(a) * x ** 2 + float(b) * x + float(c)
@@ -27,4 +26,3 @@ def plot():
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
-
